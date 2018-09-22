1496a7ee907294ae79bc9413d51375dc239ebf1f
Valery Proshchenkov <valery@proshchenkov.ru>
2018-09-22 20:02:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt -y update
-apt -y upgrade
+apt update
+apt upgrade
 
-apt -y install p7zip mc texlive-full texstudio keepassx chromium-browser openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint shotwell kontact unetbootin unrar
+apt install p7zip mc texlive-full texstudio keepassx chromium-browser openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unetbootin unrar
 
 #apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
 #echo deb http://download.onlyoffice.com/repo/debian squeeze main >> /etc/apt/sources.list
