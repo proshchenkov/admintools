78e4bcbcd7c0e7ed3cbdffdb0091b6ebbd41ee7a
proshchenkov <22844973+proshchenkov@users.noreply.github.com>
2018-12-23 14:38:36 +0300

    Update install-deb.sh

--- install-deb.sh
+++ install-deb.sh
@@ -3,6 +3,6 @@
 apt update
 apt upgrade
 
-apt install p7zip mc texlive-full texstudio keepassx chromium-browser openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unetbootin unrar inkscape openvpn
+apt install p7zip mc texlive-full texstudio keepassx chromium-browser openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unetbootin unrar inkscape openvpn gedit mtr
 
 apt autoremove
