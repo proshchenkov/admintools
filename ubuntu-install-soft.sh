81b0d0df4ac514f40f2058b00be7b86e15d6187d
proshchenkov <22844973+proshchenkov@users.noreply.github.com>
2019-03-14 21:35:57 +0300

    Update ubuntu-install-soft.sh

--- ubuntu-install-soft.sh
+++ ubuntu-install-soft.sh
@@ -3,6 +3,6 @@
 apt update
 apt upgrade
 
-apt install p7zip mc texlive-full texstudio keepassx chromium-browser openjdk-8-jdk whois samba system-config-samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unrar inkscape openvpn gedit mtr
+apt install p7zip mc texlive-full texstudio keepassx chromium-browser openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unrar inkscape openvpn gedit mtr arp-scan
 
 apt autoremove
