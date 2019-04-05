cc63efbf31dd841e3fd61fe84b2b085df3267be2
proshchenkov <22844973+proshchenkov@users.noreply.github.com>
2019-04-05 21:18:08 +0300

    Update debian-install-soft.sh

--- debian-install-soft.sh
+++ debian-install-soft.sh
@@ -3,6 +3,8 @@
 apt update
 apt upgrade
 
-apt install p7zip mc texlive-full texstudio keepassx chromium chromium-l10n openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unrar inkscape openvpn gedit mtr arp-scan thunderbird thunderbird-l10n-ru gaclculator
+apt install p7zip mc texlive-full texstudio keepassx chromium chromium-l10n openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unrar inkscape openvpn gedit mtr arp-scan thunderbird thunderbird-l10n-ru gaclculator ntp ntpdate
+
+ntpq -pn #синхронизация времени
 
 apt autoremove
