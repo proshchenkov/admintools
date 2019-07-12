dae3e5dbaac92132d1edd3f529af7088d7d13a6c
proshchenkov <22844973+proshchenkov@users.noreply.github.com>
2019-07-12 21:33:57 +0300

    Update debian-install-soft.sh

--- debian-install-soft.sh
+++ debian-install-soft.sh
@@ -5,7 +5,7 @@ apt upgrade
 
 apt install p7zip mc texlive-full texstudio keepassx chromium chromium-l10n openjdk-8-jdk whois samba vlc evince ocaml 
 ghostscript gparted htop kolourpaint4 shotwell kontact unrar inkscape openvpn gedit mtr arp-scan thunderbird thunderbird-l10n-ru 
-gaclculator ntp ntpdate netstat acl ffmpeg obs-studio net-tools
+gaclculator ntp ntpdate netstat acl ffmpeg obs-studio net-tools krita krita-l10n
 
 ntpq -pn #синхронизация времени
 
