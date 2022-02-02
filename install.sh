699c94e247645b1bafee9e85ac65106df12fae04
Valery Proshchenkov <vm.proshchenkov@gmail.com>
2022-02-02 13:38:24 +0300

    Update and rename ubuntu-install-soft.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt update && apt upgrade
+
+apt install mc keepassx vlc evince gparted htop kolourpaint4 inkscape gedit
--- ubuntu-install-soft.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt update
-apt upgrade
-
-apt install p7zip mc texlive-full texstudio keepassx chromium-browser openjdk-8-jdk whois samba vlc evince ocaml ghostscript gparted htop kolourpaint4 shotwell kontact unrar inkscape openvpn gedit mtr arp-scan
-
-apt autoremove
